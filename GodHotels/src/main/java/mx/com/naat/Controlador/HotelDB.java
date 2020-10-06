ca063fe700ad5d7f42c1d3e1c5028cc0e1425a75
MBPro-198 <mbpro-198@MacBook-Pro-de-MBPro-198.local>
2020-10-06 09:49:28 -0500

    Se agregaron los metodos necesarios para la utilizacion de la simulacion de una DB

--- GodHotels/src/main/java/mx/com/naat/Controlador/HotelDB.java
+++ GodHotels/src/main/java/mx/com/naat/Controlador/HotelDB.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mx.com.naat.Controlador;
+
+import java.util.List;
+import mx.com.naat.POJO.Cliente;
+import mx.com.naat.POJO.Habitacion;
+import mx.com.naat.POJO.Reservacion;
 
 /**
  *
- * @author mbpro-198
+ * Simulacion de la conexion a la base de datos, manejo de arreglos de las clases.
+ * @author Juan de Dios Gutierrez
+ * 
  */
 public class HotelDB {
+    
+    private List<Cliente> clientes;
+    private List<Habitacion> habitaciones;
+    private List<Reservacion> reservacions;
+    
+    /*
+        * Lista de clientes obtener.
+    */
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+    
+    /*
+        * @param Lista de clientes guardar.
+    */
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+    
+    /*
+        * @param Lista de habitacion obtener.
+    */
+    public List<Habitacion> getHabitaciones() {
+        return habitaciones;
+    }
+    
+    /*
+        * @param Lista de habitaciones guardar.
+    */
+    public void setHabitaciones(List<Habitacion> habitaciones) {
+        this.habitaciones = habitaciones;
+    }
+    
+    
+    /*
+        * @param Lista de reservacion obtener.
+    */
+    public List<Reservacion> getReservacions() {
+        return reservacions;
+    }
+    
+    /*
+        * @param Lista de reservaciones guardar.
+    */
+    public void setReservacions(List<Reservacion> reservacions) {
+        this.reservacions = reservacions;
+    }
     
 }
